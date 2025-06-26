9e98bdcee8b752f2e06e34c0479881f5613f9af4
anna <nibiruru@yandex.ru>
2025-06-26 22:17:49 +0300

    feat: add avatar

--- network/src/api/api.js
+++ network/src/api/api.js
@@ -39,6 +39,15 @@ export let profileAPI = {
     updateStatus(status) {
         return instance.put(`profile/status`, {status:status});
     },
+    savePhoto(photoFile) {
+        let formData = new FormData();
+        formData.append('image', photoFile);
+        return instance.put('profile/photo', formData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        });
+    },
 }
 
 
--- network/src/components/profile/Aboutme/Aboutme.jsx
+++ network/src/components/profile/Aboutme/Aboutme.jsx
@@ -4,6 +4,7 @@ import ptica from "../../../assets/images/ptica.jpg"; // <img className={'ptica-
 import Preloader from "../../common/Preloader";
 import ProfileStatus from "./ProfileStatus/ProfileStatus";
 import ProfileStatusWithHooks from "./ProfileStatus/ProfileStatusWithHooks";
+import usericon from '../../../assets/icons/usericon.png'
 
 const Aboutme = (props) => {
 
@@ -11,10 +12,17 @@ const Aboutme = (props) => {
         return <Preloader />
     }
 
+    const mainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            props.savePhoto(e.target.files[0])
+        }
+    }
+
     return (<div className="aboutme">
 
         <ProfileStatusWithHooks status={props.status} updateStatus={props.updateStatus}/>
-        <img src={props.profile.photos.large}/>
+        <img src={(props.profile.photos && props.profile.photos.large) ? props.profile.photos.large : usericon} alt="User avatar" />
+        {props.isOwner && <input type={'file'} onChange={mainPhotoSelected}/>}
         <p>About me</p>
 
     </div>);
